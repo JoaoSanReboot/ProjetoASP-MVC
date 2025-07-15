5311fb7be41bd135615bb5e33bfa259c1e9f2687
João Pedro <joaopedrosantanarosa@gmail.com>
2025-07-14 21:39:22 -0300

    Conexao.cs
    Criada a conexão com o SqlServer.

---
+++ ProjetoMVC/ProjetoMVC/Conexao.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjetoMVC.Data;
+
+namespace ProjetoMVC
+{
+    public class Conexao
+    {
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddEntityFrameworkSqlServer()
+                .AddDbContext<BancoContext>(o => o.UseSqlServer(Configuration.GetConnectionString("DataBase")));
+        }
+    }
+}
--- ProjetoMVC/ProjetoMVC/obj/Debug/net8.0/ProjetoMVC.AssemblyInfo.cs
+++ ProjetoMVC/ProjetoMVC/obj/Debug/net8.0/ProjetoMVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetoMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6380837c28f6f01ee25757329a6f8dfe60f7dd7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56bfd35e83891d7050c504f277d94b5591d7caf6")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetoMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetoMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
